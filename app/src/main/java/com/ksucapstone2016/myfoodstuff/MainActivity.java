90ee9b7d2d956945ebb801fe2c6971fd6e349ba9
jcaldw12 <jcaldw12@kent.edu>
2016-11-08 10:01:08 -0500

    features do not work after using search bar

--- app/src/main/java/com/ksucapstone2016/myfoodstuff/MainActivity.java
+++ app/src/main/java/com/ksucapstone2016/myfoodstuff/MainActivity.java
@@ -33,7 +33,6 @@ import android.widget.Toast;
 import android.widget.AdapterView;
 import android.widget.TextView;
 /*import android.view.View;
-
 import android.view.Menu;
 import android.view.MenuItem;*/
 import android.view.MotionEvent;
